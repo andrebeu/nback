dba57751b7248b0eefb6db75cf90d56d4387e242
andrebeu <aobeukers@gmail.com>
2019-05-10 12:43:46 -0400

    sweep pureEM ntokens

--- loop_jobsub.cmd
+++ loop_jobsub.cmd
@@ -11,7 +11,7 @@ wd_dir="/tigress/abeukers/wd/nback"
 declare -a nback_arr=(1 2 3)
 declare -a ntokens_arr=(3 4 5 10)
 
-for seed in {5..25}; do 
+for seed in {0..5}; do 
 	for nback in "${nback_arr[@]}"; do 
 		for ntokens in "${ntokens_arr[@]}"; do 
 			sbatch ${wd_dir}/gpu_jobsub.cmd "${seed}" "${nback}" "${ntokens}"
