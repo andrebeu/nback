a0c27a209400f66a1bbd2f5fd4cde829549bc03d
andrebeu <aobeukers@gmail.com>
2019-07-25 11:47:46 -0400

    .

--- Nback_models.py
+++ Nback_models.py
@@ -3,6 +3,142 @@ import numpy as np
 
 # DBG = False
 
+
+"""
+Item-recognition (sternberg) task
+"""
+
+class ItemRecognitionEM(tr.nn.Module):
+
+  """ 
+
+  """
+
+  def __init__(self,sedim,stsize,seed=155):
+    super().__init__()
+    self.mthresh = .88
+    self.sedim = sedim
+    self.stsize = stsize
+    self.cedim = 2
+    self.WM_lstm = tr.nn.LSTM(self.sedim+self.cedim,self.stsize)
+    self.ff_out = tr.nn.Linear(self.stsize,self.sedim)
+    self.initial_state = tr.rand(2,1,self.stsize,requires_grad=True)
+    return None
+
+
+  def forward_step(self,emL):
+    """ 
+    WM loop over candidate memories in emL
+    single time step 
+      takes emL [num_memories,cdim+sdim]
+      returns yhat_tstep [1,sedim]
+    """
+    # if DBG: print('WM')
+    h_t,c_t = self.initial_state
+    emL = tr.unsqueeze(emL,1)
+    yhat_tstep,(h_t,c_t) = self.WM_lstm(emL,(h_t.unsqueeze(0),c_t.unsqueeze(0)))
+    yhat_tstep = yhat_tstep[-1]
+    yhat_tstep = self.ff_out(yhat_tstep).relu()
+    return yhat_tstep
+
+  def forward_trial(self,context_trial):
+    """ 
+    input: trial context data 
+      C: `[probe,cedim]
+    loop over probes (timestep) in trial
+      query EM with context_t 
+        returns a variable length emL
+          each em is a cat([context,stim])
+        sorted according to similarity
+          ensures current context always presented at beginning
+      emL sequentially fed through WM lstm
+    """
+    setsize = context_trial.shape[0]
+    yhat_trial = -tr.ones(setsize,self.sedim)
+    for tstep in range(setsize):
+      # if DBG: print('-probe',tstep)
+      # retrieve EM
+      em_query = context_trial[tstep]
+      emL = self.retrieve_sort_sim(em_query)
+      # if DBG: print('   len_emL',emL.shape)
+      # feed WM
+      yhat_tstep = self.forward_step(emL)
+      yhat_trial[tstep] = yhat_tstep
+      # if DBG: print('yh_ts:',yhat_tstep.shape)
+    return yhat_trial
+
+  def forward(self,context_arr,stim_arr):
+    """
+    input: episode data 
+      S,C: `[trial,probe,edim]
+    output: episode_yhat 
+      Y: `[trial,probe,sdim]`
+    loop over trials (sequence of probes of len `setsize`):
+      encode trial data in EM
+      forward prop trial
+
+    """
+    # task params
+    ntrials = context_arr.shape[0]
+    setsize = context_arr.shape[1]
+    # initialize EM
+    self.EM_K = tr.Tensor([])
+    self.EM_V = tr.Tensor([])
+    # unroll (multi_trial) episode 
+    yhat_episode = -tr.ones(ntrials,setsize,self.sedim)
+    # episode loop
+    for trial in range(ntrials):
+      # if DBG: print('\n--trial',trial)
+      trial_context = context_arr[trial]
+      trial_stim = stim_arr[trial]
+      # encode EM
+      self.encode(trial_context,trial_stim)
+      # trial loop
+      yhat_trial = self.forward_trial(trial_context) # [setsize,sedim]
+      # if DBG: print('yh_tr:',yhat_trial.shape)
+      yhat_episode[trial] = yhat_trial
+    return yhat_episode
+
+
+  def encode(self,trial_context,trial_stim):
+    # if DBG: print('E')
+    self.EM_K = tr.cat([self.EM_K,
+                  trial_context
+                  ],0)
+    self.EM_V = tr.cat([self.EM_V,
+                  tr.cat([trial_context,trial_stim],-1)
+                  ],0)
+    return None
+
+  def retrieve_sort_sim(self,query):
+    """ returns retrieved memories
+    takes
+      query `(1,indim)`
+    returns 
+      emL: `num_memories,memory_dim`
+      memory_dim = sdim+cdim
+    """
+    # if DBG: print('R')
+    # L2 distance
+    dist = tr.nn.modules.distance.PairwiseDistance(2)
+    sim = 1-dist(self.EM_K,query)
+    # sort contents of EM according sim
+    sorted_sim,sort_idx = tr.sort(sim,descending=True)
+    sorted_EM_V = self.EM_V[sort_idx]
+    # above threshold indices
+    retrieve_idx = sorted_sim > self.mthresh
+    # retrievals
+    emL = sorted_EM_V[retrieve_idx]
+    return emL
+
+
+
+
+
+""" 
+Serial recall
+"""
+
 class SerialRecallEM(tr.nn.Module):
 
   """ 
